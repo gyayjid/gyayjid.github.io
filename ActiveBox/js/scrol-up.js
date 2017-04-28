7c930dcf66c8603c44669809f0ac31d6197bb755
gyayjid <maximball92@gmail.com>
2017-04-29 02:35:36 +0300

    de;ate

--- ActiveBox/js/scrol-up.js
+++
@@ -1,8 +0,0 @@
-function up() {  
-		  var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);  
-		if(top > 0) {  
-		  window.scrollBy(0,((top+100)/-15));  
-		  t = setTimeout('up()',20);  
-		} else clearTimeout(t);  
-		return false;  
-		} 
